94ac2c95d41ffe2b75895002040a4c5547662c21
Zaworov <zavvorov@gmail.com>
2020-02-08 01:36:58 +0100

    Added storage classes

---
+++ src/main/java/com/uploader/UploaderApplication.java
@@ -0,0 +1,26 @@
+package com.uploader;
+
+import com.uploader.storage.StorageProperties;
+import com.uploader.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploaderApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
